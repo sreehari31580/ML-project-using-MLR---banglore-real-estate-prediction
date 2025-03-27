361fe8bd484853db74d67ba51821af64c71b8bc3
sreehari31580 <sreeharivs31580@gmail.com>
2025-03-27 11:03:03 +0530

    Updated settings.py for deployment configuration

--- realestate/settings.py
+++ realestate/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f6cgmn%vrt&14@fd0xr41#w!7+u#$5ckc=cnr^i$e(^t_9&%iy'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-f6cgmn%vrt&14@fd0xr41#w!7+u#$5ckc=cnr^i$e(^t_9&%iy')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['onrender.com', 'localhost', '127.0.0.1', '*']
 
 
 # Application definition
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
